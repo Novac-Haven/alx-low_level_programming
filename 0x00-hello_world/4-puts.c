830eb7ad551da73e2c77b67ba0a0ed2a67084e14
novac_haven <linusabdulai17@gmail.com>
2022-12-08 21:25:34 -0800

    betty is something else

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  *main-Entry point
-  Descriptor:prints "\"Programming is like building a multilingual puzzle"
+ *Descriptor:prints "\"Programming is like building a multilingual puzzle"
  *Return: 0
  */
 int main(void)
